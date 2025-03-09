3826f1a9f9f2d58883f21268798dd973022e35e4
shaz-aftab <shazaftab20@gmail.com>
2025-03-09 23:01:17 +0000

    Used LabelEncoder & Normalized data using min-max scaling

--- src/tree.py
+++ src/tree.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn import tree
 from sklearn.tree import DecisionTreeClassifier
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv("MOCK_DATA.csv") # reading the CSV file
 
@@ -19,23 +21,26 @@ def time_to_minutes(time_str):
     except (ValueError, AttributeError):
         return None  # Skip non-convertible values
     
-# Remapping
+# Converting non-integer attributes into integers
+
+label_encoder = LabelEncoder()
+
 
 df["time"] = df["time"].apply(time_to_minutes)
-d = { 'Phone': 0, 'Shower': 1, 'Eat' : 2, 'Dress' : 3, 'Excercise' : 4, 'Meditate' : 5 }
-df['firstThing'] = df['firstThing'].map(d)
-d = { 'Snowy': 0, 'Clear': 1, 'Rainy' : 2, 'Sunny' : 3, 'Cloudy' : 4, 'Foggy' : 5, 'Hail' : 6 }
-df['weather'] = df['weather'].map(d)
-d = { False : 0, True : 1 }
-df['goodBad'] = df['goodBad'].map(d)
-d = { 'refreshed' : 0, 'tired' : 1, 'happy' : 2, 'lazy' : 3, 'groggy' : 4, 'sad' : 5 }
-df['mood'] = df['mood'].map(d)
+df['firstThing'] = label_encoder.fit_transform(df["firstThing"])
+df['weather'] = label_encoder.fit_transform(df["weather"])
+df['goodBad'] = label_encoder.fit_transform(df["goodBad"])
+df['mood'] = label_encoder.fit_transform(df["mood"])
+
 
 # specifying features
 features = ['time', 'sleepHours', 'firstThing', 'weather', 'mood']
 
 # creating seperate variables for features & target
-X = df[features]
+
+scaler = MinMaxScaler()
+
+X = scaler.fit_transform(df[features]) # Normalizing 
 y = df['goodBad']
 
 # Using Gini = 1 - (x/n)2 - (y/n)2 to make the actual decision tree
@@ -53,10 +58,10 @@ def showTree():
 def predict(): 
     prediction = dtree.predict([[
                             time_to_minutes('11:00'), # Time you wake up (4:00 to 14:00)
-                            10, # Amount of hours you slept (4 to 14 Hours)
-                            0, # First thing you do in the morning { 'Phone': 0, 'Shower': 1, 'Eat' : 2, 'Dress' : 3, 'Excercise' : 4, 'Meditate' : 5 }
-                            1, # Weather on that Day { 'Snowy': 0, 'Clear': 1, 'Rainy' : 2, 'Sunny' : 3, 'Cloudy' : 4, 'Foggy' : 5, 'Hail' : 6 }
-                            5, # Mood when you wake up { 'refreshed' : 0, 'tired' : 1, 'happy' : 2, 'lazy' : 3, 'groggy' : 4, 'sad' : 5 }
+                            14, # Amount of hours you slept (4 to 14 Hours)
+                            3, # First thing you do in the morning { 'Phone': 0, 'Shower': 1, 'Eat' : 2, 'Dress' : 3, 'Excercise' : 4, 'Meditate' : 5 }
+                            2, # Weather on that Day { 'Snowy': 0, 'Clear': 1, 'Rainy' : 2, 'Sunny' : 3, 'Cloudy' : 4, 'Foggy' : 5, 'Hail' : 6 }
+                            0, # Mood when you wake up { 'refreshed' : 0, 'tired' : 1, 'happy' : 2, 'lazy' : 3, 'groggy' : 4, 'sad' : 5 }
                         ]])
     # [0] GOOD
     # [1] BAD
@@ -65,5 +70,5 @@ def predict():
     else:
         return 'Based on what you\'ve told me I believe you day will be BAD.'
     
-#showTree()
+showTree()
 #print(predict())
